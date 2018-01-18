1697603960139add4b15d34e52e62249f2fe6513
Scot Meiste <scotm@genebygene.com>
2018-01-18 15:20:05 -0600

    added identity endpoint

---
+++ GxG.AuthWebTest/Controllers/IdentityController.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GxG.AuthWebTest.Controllers
+{
+    [Route("identity")]
+    public class IdentityController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+    }
+}
